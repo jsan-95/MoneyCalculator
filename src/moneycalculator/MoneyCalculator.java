7f535fa4a611d2043e3ba3b628b7fef1176f4ff4
Yisus95 <Yisus95@192.168.1.131>
2018-01-10 10:39:41 +0000

    MoneyCalculator versión 2.1

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -6,45 +6,49 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-
 public class MoneyCalculator {
+
     public static void main(String[] args) throws Exception {
       MoneyCalculator moneyCalculator = new MoneyCalculator();
-      moneyCalculator.execute();
+      moneyCalculator.execute();       
     }
 
     private double amount;
     private double exchangeRate;
+    private String currencyFrom;
     
-    private void execute () throws Exception{
-      input();
-      process();
-      output();
+    private void execute() throws Exception{
+        input();
+        process();
+        output();
     }
-    
+
     private void input(){
-      System.out.println("Introduzca una cantidad de dolares");
-      Scanner scanner = new Scanner(System.in);
-      amount = Double.parseDouble(scanner.next());   
+        System.out.println("Introduzca cantidad");
+        Scanner scanner = new Scanner(System.in);
+        amount = Double.parseDouble(scanner.next());
+        
+        System.out.println("Introduzca divisa inicial");
+        currencyFrom = scanner.next();   
     }
     
     private void process() throws Exception{
-      exchangeRate = getExchangeRate("USD", "EUR");
+        exchangeRate = getExchangeRate(currencyFrom,"EUR");
     }
     
     private void output(){
-      System.out.println(amount + " USD equivalen a " + amount * exchangeRate + "EUR");  
+        System.out.println(amount + " " + currencyFrom + " equivalen a " + amount * exchangeRate + " EUR");
     }
     
     private static double getExchangeRate(String from, String to) throws Exception{
-        URL url = new URL("http://api.fixer.io/latest?base=" + from + "&symbols=" + to);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        InputStreamReader input = new InputStreamReader(connection.getInputStream());
-        try (BufferedReader reader = new BufferedReader(input)){
-            String line = reader.readLine();
-            line = line.substring(line.indexOf(to)+5, line.indexOf("}"));
-            return Double.parseDouble(line);
-        }
+      URL url = new URL("http://api.fixer.io/latest?base=" + from + "&symbols=" + to);
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      InputStreamReader input = new InputStreamReader(connection.getInputStream());
+      try (BufferedReader reader = new BufferedReader(input)){
+        String line = reader.readLine();
+        line = line.substring(line.indexOf(to)+5, line.indexOf("}"));
+        return Double.parseDouble(line);
+      }
     }
 }
